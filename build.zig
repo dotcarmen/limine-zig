c90d6cd537950d295538fc913372c26783cc0ee2
Carmen <carmen@dotcarmen.dev>
2025-07-22 02:04:43 +0200

    eliminate deprecateds

--- build.zig
+++ build.zig
@@ -2,11 +2,9 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const api_revision = b.option(u32, "api_revision", "Limine API revision to use");
-    const allow_deprecated = b.option(bool, "allow_deprecated", "Whether to allow deprecated features");
 
     const config = b.addOptions();
     config.addOption(u32, "api_revision", api_revision orelse 0);
-    config.addOption(bool, "allow_deprecated", allow_deprecated orelse false);
 
     const module = b.addModule("limine", .{
         .root_source_file = b.path("src/root.zig"),
